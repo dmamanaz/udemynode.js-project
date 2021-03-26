0d8be8694b5dfd7af4c056476ee6b6c97e5ecdab
Doniyorbek Mamanazarov <doniyorad@gmail.com>
2021-03-26 00:18:30 -0400

    start project

--- server.js
+++ server.js
@@ -9,3 +9,9 @@ const app = express()
 
 const PORT = process.env.PORT||5000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
+
+//Routes here
+app.get('/', (req,res) => {
+//res.send({name:"Doni"})
+res.sendStatus(200).json({success: true, data: {id:1}});//sending status
+})
